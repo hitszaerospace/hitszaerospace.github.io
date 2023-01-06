7020e8c1a9799c3e30439d1dacf0df77c93181b8
Lynn <hitszaerospace@163.com>
2023-01-06 20:09:52 +0800

    commit

--- docs/sw.js
+++ docs/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20230106.171754';
+const cacheName = 'chirpy-20230106.200604';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {
